ffa4daa82ffabc6af966a80b2719f38e0085647a
Bhumin Bhandari <bhandaribhumin@gmail.com>
2020-11-03 20:30:30 +0530

    init project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Inspiration: https://dribbble.com/shots/8257559-Movie-2-0
+ *
+ */
+import * as React from 'react';
+
+import {
+  Animated,
+  Dimensions,
+  FlatList,
+  Image,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import Backdrop from './componenets/Backdrop'
+import { LinearGradient } from 'expo-linear-gradient';
+import { SLIDER_DATA } from './slider_data';
+import Swiper from './Swiper';
+
+const { width, height } = Dimensions.get('window');
+
+const SPACING = 5;
+const ITEM_SIZE = Platform.OS === 'ios' ? width * 0.72 : width * 0.72;
+const EMPTY_ITEM_SIZE = (width - ITEM_SIZE) / 2;
+const BACKDROP_HEIGHT = height;
+
+const Loading = () => (
+  <View style={styles.loadingContainer}>
+    <Text style={styles.paragraph}>Loading...</Text>
+  </View>
+);
+
 
 export default function App() {
+  const [slider, setSlider] = React.useState<any>([]);
+  
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+ 
+  React.useEffect(() => {
+    if (slider.length === 0) {
+      setSlider([{ key: 'empty-left' }, ...SLIDER_DATA, { key: 'empty-right' }]);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Swiper slider={slider} scrollX={scrollX} ITEM_SIZE={ITEM_SIZE}/> */}
+      <Backdrop slider={slider} scrollX={scrollX} ITEM_SIZE={ITEM_SIZE} />
+      <StatusBar hidden />
+      <Animated.FlatList
+        showsHorizontalScrollIndicator={false}
+        data={slider}
+        keyExtractor={(item) => item.key}
+        horizontal
+        bounces={false}
+        decelerationRate={Platform.OS === 'ios' ? 0 : 0.98}
+        renderToHardwareTextureAndroid
+        contentContainerStyle={{ alignItems: 'flex-end' }}
+        snapToInterval={ITEM_SIZE}
+        snapToAlignment='start'
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
+        scrollEventThrottle={16}
+        renderItem={({ item, index }) => {
+          if (!item.poster) {
+            return <View style={{ width: EMPTY_ITEM_SIZE }} />;
+          }
+          return (
+            <View style={{ width: ITEM_SIZE }}>
+              <Animated.View
+                style={{
+                  marginHorizontal: SPACING,
+                  padding: SPACING,
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                  borderRadius: 34,
+                }}
+              >
+                <Image
+                  source={{ uri: item.poster }}
+                  style={styles.posterImage}
+                />
+              </Animated.View>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  loadingContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  posterImage: {
+    width: '100%',
+    height: ITEM_SIZE * 0.75,
+    resizeMode: 'cover',
+    borderRadius: 24,
+    margin: 0,
+    marginBottom: 10,
   },
 });
---
+++ componenets/Backdrop.tsx
@@ -0,0 +1,101 @@
+/**
+ * Inspiration: https://dribbble.com/shots/8257559-Movie-2-0
+ *
+ */
+
+import * as React from "react";
+
+import {
+  Animated,
+  Dimensions,
+  FlatList,
+  Image,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import {Islider} from "./../slider_data"
+import { LinearGradient } from "expo-linear-gradient";
+import Swiper from "../Swiper";
+import { useValue } from "react-native-reanimated";
+
+interface BackdropProp {
+  slider: Islider;
+  scrollX: any;
+  ITEM_SIZE: number;
+}
+const { width, height } = Dimensions.get("window");
+const Backdrop = ({ slider, scrollX, ITEM_SIZE }: BackdropProp) => {
+  const translateX = useValue(0);
+  const snapPoints = slider.map((_, i: number) => i * -width);
+  console.log('width',width);
+  return (
+   
+    <View  style={styles.container}>
+      <Animated.View style={StyleSheet.absoluteFill}>
+      <FlatList
+        data={slider.reverse()}
+        keyExtractor={(item) => item.key + "-backdrop"}
+        removeClippedSubviews={false}
+        contentContainerStyle={{ width, height: height }}
+        renderItem={({ item, index }) => {
+          if (!item.backdrop) {
+            return null;
+          }
+          const translateX = scrollX.interpolate({
+            inputRange: [(index - 2) * ITEM_SIZE, (index - 1) * ITEM_SIZE],
+            outputRange: [0, width],
+            // extrapolate:'clamp'
+          });
+          return (
+            <Animated.View
+              removeClippedSubviews={false}
+              style={{
+                position: "absolute",
+                width: translateX,
+                height,
+                overflow: "hidden",
+              }}
+            >
+              <Image
+                source={{ uri: item.backdrop }}
+                style={{
+                  width,
+                  height: height,
+                  position: "absolute",
+                }}
+              />
+            </Animated.View>
+          );
+        }}
+      />
+      </Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "black",
+  },
+  pictures: {
+    height,
+    flexDirection: "row",
+  },
+  picture: {
+    width,
+    height,
+    overflow: "hidden",
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    width: undefined,
+    height: undefined,
+  },
+});
+export default Backdrop;
